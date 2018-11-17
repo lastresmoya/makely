94d27ba4fe9c170118c33f38ee5478696445af36
lastresmoya <lastresmoyallc@gmail.com>
2018-11-17 12:15:03 +0800

    all jobs ui front end

--- src/js/components/Navs/Breadcrumbs/index.js
+++ src/js/components/Navs/Breadcrumbs/index.js
@@ -9,7 +9,7 @@ function Breadcrumbs(props) {
         <div className="bg-light card m-0">
             <div className="container">
                 <nav aria-label="breadcrumb">
-                    <ol className="breadcrumb m-0">
+                    <ol className="breadcrumb m-0 px-0">
                         <li className="breadcrumb-item"><Link to={"/"}><FaIcon icon={faHome} /></Link></li>
                         <li className="breadcrumb-item active" aria-current="page">All Jobs</li>
                     </ol>
--- src/js/components/Type/PageTitle/index.js
+++ src/js/components/Type/PageTitle/index.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function PageTitle({title}) {
     return (
-        <div>
-            {title}
+        <div className="card">
+            <div className="card-body pb-2 pt-3">
+                <div className="container">
+                    <h3 className="font-weight-bold">{title}</h3>
+                </div>
+            </div>
         </div>
     );
 }
