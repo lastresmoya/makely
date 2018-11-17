76cbb5eef646ff5aebe7b79c98fec1bddd928515
lastresmoya <lastresmoyallc@gmail.com>
2018-11-18 02:22:39 +0800

    wip messages

---
+++ src/js/pages/private/Profile/MessagesPage/MessageJob/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function MessageJob(props) {
+    return (
+        <div className="card-body bg-light d-flex justify-content-between align-items-start">
+            <div>
+                <p className="card-text font-weight-bold mb-1">Job title, Nam porttitor blandit accumsan.</p>
+                <p className="card-text font-weight-bold mb-1">Quantity: <span className="text-muted">2</span></p>
+                <p className="card-text font-weight-bold mb-1">Total Cost: <span className="text-muted">£50.00</span></p>
+                <p className="card-text font-weight-bold mb-1">Materials: <span className="text-muted">PVC, ABS</span></p>
+                <span className="h5"><span className="text-uppercase badge badge-warning">NEGOTIATION</span></span>
+            </div>
+            <div>
+                <button className="btn btn-secondary px-3 rounded-lg mr-3">ACCEPT</button>
+                <button className="btn btn-outline-secondary px-3 rounded-lg"><span className="text-muted">CANCEL</span></button>
+            </div>
+        </div>
+    );
+}
+
+export default MessageJob;
--- src/js/pages/private/Profile/MessagesPage/index.js
+++ src/js/pages/private/Profile/MessagesPage/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Breadcrumbs from "Components/Navs/Breadcrumbs";
 import PageTitle from "Components/Type/PageTitle";
 import MessageItem from "./MessageItem";
+import MessageJob from "./MessageJob";
+import avatarAlice from "./assets/avatar-alice.png";
+import avatarMark from "./assets/avatar-mark.png";
 import FaIcon from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/fontawesome-free-solid';
+import { faSearch, faPlus, faTimesCircle } from '@fortawesome/fontawesome-free-solid';
 
 
 function MessagesPage (props) {
@@ -38,7 +41,51 @@ function MessagesPage (props) {
                         </div>
                     </div>
                     <div className="col-sm-8">
+                        <div className="card shadow-sm">
+                            <div className="card-header">
+                                <h5 className="card-title m-0">Messages</h5>
+                            </div>
+                            <MessageJob/>
+                            <div className="card-body message-box">
+                                {/* Alice */}
+                                <div className="d-flex align-items-start mb-4">
+                                    <img src={avatarAlice} alt=""/>
+                                    <div className="px-3 pt-3 pb-4 ml-3 mr-5 bg-light-secondary card shadow-sm border-0">
+                                        <div className="d-flex justify-content-between"><strong>Alice Smith</strong> <strong className="text-muted">5:45pm</strong></div>
+                                        <p className="m-0">In malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ip</p>
+                                    </div>
+                                </div>
+                                
+                                {/* Me Mark */}
+                                <div className="d-flex align-items-start mb-4">
+                                    <div className="px-3 pt-3 pb-4 mr-3 ml-5 bg-light card shadow-sm border-0">
+                                        <div className="d-flex justify-content-between"><strong className="text-muted">5:45pm</strong><strong>You</strong></div>
+                                        <p className="m-0">In malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. Nullam vehicula nisi velit. Mauris turpis nisl, molestie ut ip</p>
+                                    </div>
+                                    <img src={avatarMark} alt="" />
+                                </div>
 
+                                {/* Me Mark Declined */}
+                                <div className="d-flex align-items-start mb-4">
+                                    <div className="px-3 pt-3 pb-4 mr-3 ml-5 bg-light-danger card shadow-sm border-0 d-flex justify-content-start flex-row">
+                                        <div className="h5 text-danger mr-3"><FaIcon icon={faTimesCircle}/></div>
+                                        <div className="w-100">
+                                            <strong>You have declined this project</strong>
+                                            <p className="m-0"><strong>Reason:</strong> In malesuada enim in dolor euismod, id commodo mi consectetur. Curabitur at vestibulum nisi. </p>
+                                        </div>
+                                    </div>
+                                    <img src={avatarMark} alt="" />
+
+                                </div>
+                            </div>
+                            <div className="card-footer p-0">
+                                <textarea name="" className="form-control border-0" rows="3" defaultValue="Start typing here..."></textarea>
+                            </div>
+                            <div className="card-footer bg-light d-flex justify-content-between align-items-center py-1">
+                                <button className="btn btn-link px-0 text-muted font-weight-bold"><FaIcon icon={faPlus} /><span className="ml-2">Attach Files</span></button>
+                                <div className="card-text text-muted">Press enter to send</div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
