507f843edb3f58f20b312d050bc45eaa020f4dd7
lastresmoya <lastresmoyallc@gmail.com>
2018-12-06 11:24:28 -0600

    added styles for image (creator list item)

--- src/js/pages/public/FindCreatorsPage/CreatorsList/CreatorListItem/index.js
+++ src/js/pages/public/FindCreatorsPage/CreatorsList/CreatorListItem/index.js
@@ -28,7 +28,7 @@ class CreatorListItem extends React.Component {
         return (
             <div className="card creator-list__item mb-3 shadow-sm card-creator position-relative pb-4 pb-md-0">
                 <div className="card-body p-2 d-md-flex justify-content-start align-items-stretch">
-                    <img className="w-md-50 mx-auto mx-md-0  d-block d-md-line mb-4 mb-md-0" src={creatorThumb} alt="" />
+                    <img className="w-md-50 mx-auto mx-md-0  d-block d-md-line mt-3 mt-md-0 mb-4 mb-md-0" src={creatorThumb} alt="" />
                     <div className="px-3 w-100 position-relative">
                         <div className="d-md-flex align-items-center justify-content-start pt-0 pb-1">
                             <span className="font-weight-bold h4 mr-3 mb-0">John Brewer</span>
