efb0150f4a9c2ce57aad0bc256d73e365e4fdffb
lastresmoya <lastresmoyallc@gmail.com>
2018-12-11 13:22:06 +0800

    FAQ WIP

--- src/js/pages/public/FAQsPage/index.js
+++ src/js/pages/public/FAQsPage/index.js
@@ -1,9 +1,32 @@
 import React from "react";
+import PageTitle from "Components/Type/PageTitle";
+import FAQItem from "Components/FAQItem";
+
 
 function FAQsPage(props) {
+    const faq1 = {
+        id: 1,
+        title: "What is Makely",
+        content: "Lorem"
+    }
     return (
         <div>
-            FAQs Page
+            <PageTitle title="Frequently Asked Questions" />
+            <div className="container py-4">
+                <section className="text-center py-4">
+                    <h2 className="font-weight-bold">Clients</h2>
+                    <div className="divider"></div>
+                    <FAQItem {...faq1}/>
+                </section>
+                <section className="text-center py-4">
+                    <h2 className="font-weight-bold">Creators</h2>
+                    <div className="divider"></div>
+                </section>
+                <section className="text-center py-4">
+                    <h2 className="font-weight-bold">Misc</h2>
+                    <div className="divider"></div>
+                </section>
+            </div>
         </div>
     );
 }
