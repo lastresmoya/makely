bb2e470b67983c7dc25a332846bc5f67cecf0ad0
lastresmoya <lastresmoyallc@gmail.com>
2018-12-23 00:41:19 +0800

    signup page ui

--- src/js/components/Forms/TextInput/index.js
+++ src/js/components/Forms/TextInput/index.js
@@ -1,25 +1,30 @@
 import React from "react";
 import FaIcon from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/fontawesome-free-regular';
-import { faLock } from '@fortawesome/fontawesome-free-solid';
+import { faLock, faUser } from '@fortawesome/fontawesome-free-solid';
 
 
-function TextInput({type, placeholder}) {
+function TextInput({type, placeholder, icon}) {
     let renderIcon = () => {
-        if(type==='email') {
-            return <FaIcon icon={faEnvelope}/>
-        }
-        if (type === 'password') {
-            return <FaIcon icon={faLock} />
-        }
+        if(type==='email') return <FaIcon icon={faEnvelope}/>;
+        if (type === 'password') return <FaIcon icon={faLock} />;
+        if (icon === 'user') return <FaIcon icon={faUser} />;
     } 
+    let renderType = () => {
+        console.log(type)
+        if(type && type) {
+            return `${type}`
+        } else {
+            return "text"
+        }
+    }
     return (
         <React.Fragment>
             <div className="input-group mb-3 border-bottom">
                 <div className="input-group-prepend ">
                     <span className="input-group-text bg-transparent border-0" id={`${type}-addon`}>{renderIcon()}</span>
                 </div>
-                <input type={type} className="form-control bg-transparent border-0" placeholder={placeholder} aria-label={placeholder} aria-describedby={`${type}-addon`}/>
+                <input type={renderType()} className="form-control bg-transparent border-0" placeholder={placeholder} aria-label={placeholder} aria-describedby={`${type}-addon`}/>
             </div>
         </React.Fragment>
     );
--- src/js/pages/public/SignupPage/index.js
+++ src/js/pages/public/SignupPage/index.js
@@ -1,9 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BtnSecondary from "Components/Buttons/BtnSecondary";
+import BtnDanger from "Components/Buttons/BtnDanger";
+import TextInput from "Components/Forms/TextInput";
+import CheckInput from "Components/Forms/CheckInput";
+
 
 function SignupPage(props) {
     return (
-        <div>
-            Signup Page
+        <div className="bg-light py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-10 m-auto">
+                        <div className="card border-0 py-4 mb-5">
+                            <div className="card-body">
+                                <div className="col-sm-8 m-auto">
+                                    <section/><section/>
+                                    <section>
+                                        <h2 className="font-weight-bold text-center">Sign up and start creating</h2>
+                                        <div className="divider"></div>
+                                        <TextInput icon="user" placeholder="FIRST NAME" />
+                                        <TextInput icon="user" placeholder="LAST NAME" />
+                                        <TextInput type="email" placeholder="EMAIL" />
+                                        <TextInput type="password" placeholder="PASSWORD" />
+                                        <TextInput type="password" placeholder="CONFIRM PASSWORD" />
+                                        <p className="font-weight-bold mt-4">Account type</p>
+                                        <div className="d-flex justify-content-start align-items-center mb-4">
+                                            <div className="mr-3"><CheckInput label="CLIENT" /></div>
+                                            <div className="mr-3"><CheckInput label="CREATOR" /></div>
+                                            <div className="mr-3"><CheckInput label="BOTH" /></div>
+                                        </div>
+                                        <div className="text-center pb-4">
+                                            <Link to="/profile/general">
+                                                <BtnDanger content="SIGNUP" />
+                                            </Link>
+                                        </div>
+                                        <div className="mt-4 text-center">
+                                            <span className="mr-2">Already have an account?</span>
+                                            <Link className="btn px-0 btn-link text-secondary" to="/login">Sign in here</Link>
+                                        </div>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
