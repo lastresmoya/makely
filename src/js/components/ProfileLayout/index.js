e1cc1a6f7830e952d20ed19fc0b698d64fe5e9b0
lastresmoya <lastresmoyallc@gmail.com>
2018-12-23 02:31:46 +0800

    wip profile page, user info

--- src/js/components/ProfileLayout/index.js
+++ src/js/components/ProfileLayout/index.js
@@ -1,19 +1,59 @@
 import React from "react";
+import Breadcrumbs from "Components/Navs/Breadcrumbs";
+import PageTitle from "Components/Type/PageTitle";
+import UserInfo from "Components/User/UserInfo"
 
-function ProfileLayout({user}) {
+class ProfileLayout extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userName: 'Mark H.',
+            location: 'London, United Kingdom',
+            bio: 'Vestibulum rutrum quam vitae fringilia tinciditun. Suspendisse nex tortor urna. Ut laoreet sodales nisi, quis laculis nulla lacilis vitae.',
+            currentView: 'creator'
+        }
+    }
     
-    return (
-        <React.Fragment>
-            <h1>Esto aparece en las dos páginas</h1>
-        
-        {user==='public' && 
-            <div> Esta cajita solo aparece para el público</div>
+    render(){
+        const {user} = this.props;
+        const {userName} = this.state;
+        const userTitle = () => {
+            if (user === 'logged') { return `Your Profile`}
+            else return `${userName} Profile`;
         }
-        {user === 'logged' &&
-            <div>Esta otra caja solo aparce para si estoy loggeado</div>
-        }
-        </React.Fragment>
-    );
+        return (
+            <React.Fragment>
+                {/* Breadcrumbs */}
+                {user === 'logged' && <Breadcrumbs current={userName} />}
+                {/* Page Title */}
+                <PageTitle title={userTitle()}/>
+
+                <section className="py-4">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm-4">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <UserInfo user={user} userInfo={this.state}/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-8">
+                            
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {user === 'public' &&
+                    <div> Esta cajita solo aparece para el público</div>
+                }
+                {user === 'logged' &&
+                    <React.Fragment></React.Fragment>
+                }
+            </React.Fragment>
+        );
+    }
 }
 
 export default ProfileLayout;
